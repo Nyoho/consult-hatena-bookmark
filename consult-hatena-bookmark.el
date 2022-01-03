9157f9f02ad7eb938ff12a744a13c9e1819c60f9
北䑓如法 <algebraicallyClosedField@gmail.com>
2022-01-04 00:08:02 +0900

    Add description of license

--- consult-hatena-bookmark.el
+++ consult-hatena-bookmark.el
@@ -3,7 +3,22 @@
 ;; Copyright (C) 2021 Yukinori Kitadai
 
 ;; Author: Yukinori Kitadai
+;; Package-Requires: ((emacs "27.1") (consult "0.9"))
 ;; Version: 0.0.0
+;; URL: https://github.com/Nyoho/consult-hatena-bookmark
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Command: `consult-hatena-bookmark`
 
